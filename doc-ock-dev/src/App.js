6a743a7d4c412f0f629e5224a9b77308bd5beb8d
coderjake91 <jacob.b.frazer@gmail.com>
2022-01-01 11:03:27 -0600

    added basic file structure for react portfolio site

---
+++ doc-ock-dev/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+//import logo from './logo.svg';
+import './App.css';
+import Jacob from './components/Jacob';
+
+function App() {
+  return (
+    <div className="App">
+      <Jacob></Jacob>
+    </div>
+  );
+}
+
+export default App;
---
+++ doc-ock-dev/src/components/Jacob/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+const Jacob = () => {
+    return (
+        <div  className="jacob">
+            <p>[ softwareEngineer ] = jacobFrazer</p>
+        </div>
+    );
+}
+
+export default Jacob;
